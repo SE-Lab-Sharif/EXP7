728041bfd2fdf7c95be4f8ab3abf46076af2d081
soheilnazari2001 <soheil.nazari@sharif.edu>
2024-12-18 01:27:50 +0330

    Added Strategy Pattern to the action class

---
+++ src/main/java/MiniJava/parser/ShiftAction.java
@@ -0,0 +1,15 @@
+package MiniJava.parser;
+public class ShiftAction extends Action{
+    public ShiftAction(int number) {
+        super(number);
+    }
+    @Override
+    public void act(Parser parser) {
+        parser.getParsStack().push(this.number);
+        parser.setLookAhead(parser.getLexicalAnalyzer().getNextToken());
+    }
+    @Override
+    public String toString() {
+        return "s" + number;
+    }
+}
